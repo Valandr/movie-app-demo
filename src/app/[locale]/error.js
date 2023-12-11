4753071eb02280b8610d67a936abab94edd9434f
Valandr <valerie.andriani@gmail.com>
2023-12-11 16:39:01 +0100

    Updating error message app and building app for production

--- src/app/[locale]/error.js
+++ src/app/[locale]/error.js
@@ -5,8 +5,8 @@ import React from "react";
 export default function Error() {
   return (
     <div className="error">
-      <h1>Error</h1>
-      <p>Ouuups une erreur s&apos;est produite</p>
+      <h1>Error sorry</h1>
+      <p>Ouups une erreur s&apos;est produite</p>
     </div>
   );
 }
